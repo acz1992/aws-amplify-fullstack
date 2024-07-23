6a53ee92e8a5072d37d7390a778084aad34078da
acz19 <acz1992@hotmail.co.uk>
2024-07-23 18:36:02 +0200

    Integrate Amplify into App and add Auth frontend

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "amplifyfullstackappd73b7831": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
+import logo from "./logo.svg";
+import "@aws-amplify/ui-react/styles.css";
+/* import '@aws-amplify/ui-react/styles.css'; */
+import {
+	withAuthenticator,
+	Button,
+	Heading,
+	Image,
+	View,
+	Card,
+} from "@aws-amplify/ui-react";
 
-function App() {
-	return <div className="App">Zeh and Adams super sick website</div>;
+function App({ signOut }) {
+	return (
+		<View className="App">
+			<Card>
+				<Image src={logo} className="App-logo" alt="logo" />
+				<Heading level={1}>We now have Auth</Heading>
+			</Card>
+			<Button onClick={signOut}>Sign Out</Button>
+		</View>
+	);
 }
 
-export default App;
+// Wrap App in higher order component, so when user navigates to index URL, redirects to login page
+export default withAuthenticator(App);
